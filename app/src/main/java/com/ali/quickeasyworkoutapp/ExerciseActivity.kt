384b07e659ed9c9371d333fe00640041fe77af06
Mohsin0899 <mohsin.saifi98@gmail.com>
2022-10-16 20:55:31 +0530

    Toolbar

---
+++ app/src/main/java/com/ali/quickeasyworkoutapp/ExerciseActivity.kt
@@ -0,0 +1,23 @@
+package com.ali.quickeasyworkoutapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ali.quickeasyworkoutapp.databinding.ActivityExerciseBinding
+
+class ExerciseActivity : AppCompatActivity() {
+    private var binding: ActivityExerciseBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityExerciseBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        setSupportActionBar(binding?.toolbarExercise)
+        if(supportActionBar != null){
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+        binding?.toolbarExercise?.setNavigationOnClickListener{
+            onBackPressed()
+        }
+    }
+}
--- app/src/main/java/com/ali/quickeasyworkoutapp/MainActivity.kt
+++ app/src/main/java/com/ali/quickeasyworkoutapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.ali.quickeasyworkoutapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
@@ -14,7 +15,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding?.root)
 
         binding?.flStart?.setOnClickListener{
-            Toast.makeText(this@MainActivity, "Exercise", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, ExerciseActivity::class.java)
+            startActivity(intent)
         }
     }
 
